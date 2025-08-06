43459d697cab2db584afc43f15f5559d37624ebe
Yahaya Salisu BabsAudits <yahayasalisubabs@gmail.com>
2025-08-07 00:29:43 +0100

    Update DeployRSC.s.sol

--- ERC20/script/DeployRSC.s.sol
+++ ERC20/script/DeployRSC.s.sol
@@ -1,1 +1,17 @@
-1
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import { Script } from "forge-std/Script.sol";
+import "../src/RoseCoin.sol";
+
+contract deployRSC is Script {
+    RoseCoin public roseCoin;
+
+    function run() public {
+        
+        vm.startBroadcast();
+        roseCoin = new RoseCoin();
+        vm.stopBroadcast();
+    }
+}
