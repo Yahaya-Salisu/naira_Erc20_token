62a071e48e6ba4759003f71ed34b2f4ecf0ad486
Yahaya Salisu <yahayasalisu162@gmail.com>
2025-08-25 00:33:08 +0100

    Update folders

--- contracts/test/RoseCoin.t.sol
+++ contracts/test/RoseCoin.t.sol
@@ -5,4 +5,15 @@ pragma solidity ^0.8.20;
 import { Test } from "forge-std/Test.sol";
 import "../src/RoseCoin.sol";
 
-contract RoseCoinTest is Test {}
+contract RoseCoinTest is Test {
+    RoseCoin public rosecoin;
+
+    function setUp() public {
+        rosecoin = new RoseCoin();
+        uint256 amount = 100;
+        address User1 = makeAddr(User1);
+        address User2 = makeAddr(User2);
+    }
+
+    function test_mint() public {}
+}
